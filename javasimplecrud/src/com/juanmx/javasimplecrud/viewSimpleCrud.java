475207f2f30b904eedba20a420a90462af64df24
JuanMX <torres-olivares@hotmail.com>
2023-06-27 23:27:14 -0600

    🚧 Added Create, Edit and Delete Buttons

--- javasimplecrud/src/com/juanmx/javasimplecrud/viewSimpleCrud.java
+++ javasimplecrud/src/com/juanmx/javasimplecrud/viewSimpleCrud.java
@@ -1,11 +1,13 @@
 package com.juanmx.javasimplecrud;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.Color;
 
 //menu bar
 import javax.swing.JRadioButtonMenuItem;
@@ -15,6 +17,9 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import java.util.ArrayList;
 import javax.swing.ButtonGroup;
+
+//action buttons CRUD
+import javax.swing.JPanel;
 
 import javax.swing.JOptionPane;
 import javax.swing.JFileChooser;
@@ -27,7 +32,7 @@ import javax.swing.JFileChooser;
 
 public class viewSimpleCrud extends JFrame implements ActionListener{
 	
-	//placeholders layout
+	//place holders layout
 	private JButton buttons[];
 	private final String names[] = {"North", "South", "East", "West", "Center"};
 	private BorderLayout schema;
@@ -35,6 +40,10 @@ public class viewSimpleCrud extends JFrame implements ActionListener{
 	//menu bar set look and feel
 	private JRadioButtonMenuItem lookAndFeels[];
 	private ButtonGroup lookAndFeelsButtonGroup;
+	
+	//panel action buttons
+	private JPanel panelActionButtons;
+	private JButton actionButtons[];
 	
 	public viewSimpleCrud(String configLookAndFeel){
 		
@@ -74,9 +83,11 @@ public class viewSimpleCrud extends JFrame implements ActionListener{
 		}
 		lookAndFeels[intSelectedLookAndFeel].setSelected(true);
 		
+		
 		// Place holders for BorderLayout cardinals
 		schema = new BorderLayout(17, 5);
 		setLayout( schema );
+		/*
 		buttons = new JButton[names.length];
 		
 		for(int i=0; i<names.length; i++){
@@ -88,6 +99,29 @@ public class viewSimpleCrud extends JFrame implements ActionListener{
 		add(buttons[2], BorderLayout.EAST);
 		add(buttons[3], BorderLayout.WEST);
 		add(buttons[4], BorderLayout.CENTER);
+		*/
+		
+		//action buttons
+		actionButtons = new JButton[3];
+		panelActionButtons = new JPanel();
+		panelActionButtons.setLayout(new GridLayout(1, actionButtons.length, 17 , 1));
+		ManagerCrudButton managerCrudButton = new ManagerCrudButton();
+		
+		actionButtons[0] = new JButton("Create New");
+		actionButtons[0].setBackground(new Color(0,255,0));
+		actionButtons[0].addActionListener(managerCrudButton);
+		actionButtons[1] = new JButton("Edit Record");
+		actionButtons[1].setBackground(new Color(255,255,0));
+		actionButtons[1].addActionListener(managerCrudButton);
+		actionButtons[2] = new JButton("Delete Record");
+		actionButtons[2].setBackground(new Color(255,0,0));
+		actionButtons[2].addActionListener(managerCrudButton);
+		
+		panelActionButtons.add(actionButtons[0]);
+		panelActionButtons.add(actionButtons[1]);
+		panelActionButtons.add(actionButtons[2]);
+		
+		add(panelActionButtons, BorderLayout.SOUTH);
 	}
 	
 	public void actionPerformed(ActionEvent event){
@@ -154,6 +188,24 @@ public class viewSimpleCrud extends JFrame implements ActionListener{
 						JOptionPane.showMessageDialog( null, "A problem ocurred with the look and feel " + lookAndFeelSelected, "!", JOptionPane.ERROR_MESSAGE );
 					}
 				}
+			}
+		}
+	}
+	
+	private class ManagerCrudButton implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event) {
+			if(event.getSource() == actionButtons[0]) {
+				JOptionPane.showMessageDialog( null, "Click on create ", "!", JOptionPane.PLAIN_MESSAGE );
+			}
+			else if(event.getSource() == actionButtons[1]) {
+				JOptionPane.showMessageDialog( null, "Click on edit ", "!", JOptionPane.PLAIN_MESSAGE );
+			}
+			else if(event.getSource() == actionButtons[2]) {
+				JOptionPane.showMessageDialog( null, "Click on delete ", "!", JOptionPane.PLAIN_MESSAGE );
+			}
+			else {
+				
 			}
 		}
 	}
