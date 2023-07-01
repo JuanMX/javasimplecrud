3d5e5f174e2f6e9b55c473fa925f4ec80b1085f3
JuanMX <torres-olivares@hotmail.com>
2023-06-30 18:35:33 -0600

    🚧 Search for a image button can select and display image

--- javasimplecrud/src/com/juanmx/javasimplecrud/ViewNewRecord.java
+++ javasimplecrud/src/com/juanmx/javasimplecrud/ViewNewRecord.java
@@ -1,16 +1,25 @@
 package com.juanmx.javasimplecrud;
 
 import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
 
 /**
  * In the main view when click the button [New record]
@@ -26,6 +35,7 @@ public class ViewNewRecord extends JFrame{
 	private GridBagLayout schema;
 	private GridBagConstraints restrictions;
 	private JLabel imageLabel, descriptionLabel, keyWordsLabel;
+	JButton buttonChooseImage = new JButton("Search for a image");
 	
 	public ViewNewRecord() {
 		super("New Record");
@@ -41,34 +51,36 @@ public class ViewNewRecord extends JFrame{
 		JComboBox boxComb = new JComboBox(names);
 		
 		JTextField fieldText = new JTextField("Field Text");
-		JButton buttonChooseImage = new JButton("Search for a image");
-		buttonChooseImage.setToolTipText("Search for a image is optional");
+		
+		buttonChooseImage.setToolTipText("Optional Field");
 		JButton buttonSave = new JButton("Save New Record");
 		buttonSave.setBackground(new Color(0,255,255));
 		
 		// left element preview image
 		restrictions.fill = GridBagConstraints.BOTH;
-		restrictions.weightx = 40;
-		restrictions.weighty = 25;
+		restrictions.weightx = 27;
+		restrictions.weighty = 100;
 		imageLabel = new JLabel();
-		addComponent(imageLabel, 0, 0, 1, 11);
+		addComponent(imageLabel, 0, 0, 2, 11);
 		
 		restrictions.weightx = 75;
-		restrictions.weighty = 75;
+		restrictions.weighty = 100;
 		
 		restrictions.fill = GridBagConstraints.HORIZONTAL;
 		descriptionLabel = new JLabel("Description:");
 		addComponent(descriptionLabel, 0, 3, 3, 1);
 		restrictions.fill = GridBagConstraints.BOTH;
-		addComponent( areaTextDescription, 1, 4, 3, 4);
+		addComponent( new JScrollPane(areaTextDescription), 1, 4, 3, 4);
 		
-		restrictions.fill = GridBagConstraints.HORIZONTAL;
+		restrictions.fill = GridBagConstraints.BOTH;
 		keyWordsLabel = new JLabel("Key Words:");
 		addComponent( keyWordsLabel, 5, 3, 3, 1);
-		addComponent( areaTextKeyWords, 6, 4, 3, 2);
+		addComponent(  new JScrollPane(areaTextKeyWords), 6, 4, 3, 2);
 		
 		
 		restrictions.fill = GridBagConstraints.HORIZONTAL;
+		ManagerImageButton managerImageButton = new ManagerImageButton();
+		buttonChooseImage.addActionListener(managerImageButton);
 		addComponent(buttonChooseImage, 9, 4, 2, 1);
 		restrictions.fill = GridBagConstraints.NONE;
 		addComponent(buttonSave, 10, 4, 2, 1);
@@ -90,5 +102,32 @@ public class ViewNewRecord extends JFrame{
 		restrictions.gridheight = height;
 		schema.setConstraints(component, restrictions);
 		add(component);
+	}
+	
+	private class ManagerImageButton implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event) {
+			
+			if(event.getSource() == buttonChooseImage) {
+				
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Select a jpg or png image");//title JFileChooser
+				fileChooser.setFileFilter(new FileNameExtensionFilter("JPG and PNG Images", "jpg", "JPG", "jpeg", "JPEG", "png", "PNG"));
+				int seleccion = fileChooser.showOpenDialog(null);
+				
+				  
+				if (seleccion == JFileChooser.APPROVE_OPTION) {
+					
+					File fileToSave = fileChooser.getSelectedFile();
+					//textSonido.setText("");
+					//textSonido.setText(fileToSave.getAbsolutePath());
+					String imageAbsolutePath = fileToSave.getAbsolutePath();
+					ImageIcon labelImage = new ImageIcon(new ImageIcon(imageAbsolutePath).getImage().getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_DEFAULT));
+					imageLabel.setIcon(labelImage);
+					//JOptionPane.showMessageDialog( null, "File name: " + imageAbsolutePath, "You selected", JOptionPane.INFORMATION_MESSAGE );
+				}
+			}
+			
+		}
 	}
 }
